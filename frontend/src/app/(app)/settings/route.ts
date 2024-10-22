a82baa6ed61132111c17bb4cf555ad8b45c8ec6a
Greg Murray <gd-murray@hotmail.com>
2024-10-22 16:17:46 -0700

    settings page redirect

--- frontend/src/app/(app)/settings/route.ts
+++ frontend/src/app/(app)/settings/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(request: NextResponse) {
+export async function GET(request: NextRequest) {
   const baseUrl = new URL(request.url).origin
   return NextResponse.redirect(`${baseUrl}/settings/general`)
 }
